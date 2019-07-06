5095aa4ae1fb95e6686f229822933136695bda6b
Sarah Drasner <sarah.drasner@gmail.com>
2019-07-05 21:38:17 -0600

    start working on the cart

---
+++ pages/cart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <app-card />
+  </div>
+</template>
+
+<script>
+import AppCard from "~/components/AppCard.vue";
+
+export default {
+  components: {
+    AppCard
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
